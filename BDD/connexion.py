3acd96f5efb8761c2d98bc654597a137299a28c8
BouzSam <sbouzidi@franprix.fr>
2026-05-11 17:12:25 +0200

    Support Hugging Face sans PostgreSQL

--- BDD/connexion.py
+++ BDD/connexion.py
@@ -1,62 +1,62 @@
 # BDD/connexion.py
 
-# create_engine :
-# fabrique l'objet qui sait se connecter à PostgreSQL
 from sqlalchemy import create_engine
-
-# sessionmaker :
-# fabrique des "sessions", donc des connexions de travail vers la base
 from sqlalchemy.orm import sessionmaker
 
-# Base :
-# contient les tables définies dans models.py
 from BDD.models import Base
 
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-# engine :
-# connexion principale à la base
-# SQLAlchemy s'en sert pour envoyer les requêtes
-engine = create_engine(DATABASE_URL)
+# Si DATABASE_URL existe
+# -> PostgreSQL activé
+if DATABASE_URL:
 
+    engine = create_engine(DATABASE_URL)
 
-# SessionLocal :
-# "usine" qui crée des sessions
-# une session = ce qu'on utilise pour lire/écrire dans la base
-SessionLocal = sessionmaker(
-    autocommit=False,  # on valide nous-mêmes avec db.commit()
-    autoflush=False,   # n'envoie pas auto les changements tout de suite
-    bind=engine,       # la session utilisera cette connexion
-)
+    SessionLocal = sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=engine,
+    )
+
+# Sinon (ex : Hugging Face)
+# -> PostgreSQL désactivé
+else:
+
+    engine = None
+    SessionLocal = None
 
 
 def create_tables() -> None:
-    # crée toutes les tables définies dans Base
-    # ici Base connaît InputData et PredictionResult
-    Base.metadata.create_all(bind=engine)
-    print("Succès : les tables sont créées.")
+
+    # Création des tables SQLAlchemy
+    if engine is not None:
+        Base.metadata.create_all(bind=engine)
+        print("Succès : les tables sont créées.")
 
 
 def get_db():
-    # ouvre une session
+
+    # Si pas de base PostgreSQL
+    if SessionLocal is None:
+        yield None
+        return
+
+    # Sinon ouverture session DB
     db = SessionLocal()
 
     try:
-        # donne la session à FastAPI
         yield db
 
     finally:
-        # ferme toujours la session à la fin
         db.close()
 
 
 if __name__ == "__main__":
-    # si tu lances ce fichier directement :
-    # python BDD/connexion.py
-    # alors les tables seront créées
     create_tables()
